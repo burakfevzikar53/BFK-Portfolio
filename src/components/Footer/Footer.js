29849bae627441060e8ac3439dcaf2a6ddb588c5
BFK <49106046+burakfevzikar53@users.noreply.github.com>
2024-12-05 21:59:41 +0300

    Add footer lang

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./Footer.css";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { useTheme } from "../../context/ThemeContext";
+import { useLanguage } from "../../context/LanguageContext";
 
 const Footer = () => {
   const { theme } = useTheme();
-
+  const { language } = useLanguage();
   return (
     <footer className={`footer ${theme === "dark" ? "dark-theme" : "light-theme"}`}>
-      <p>&copy; 2024 Your Name. All Rights Reserved.</p>
+      <p>&copy; {language === "en" ? "2025 Burak Fevzi Kar. All Rights Reserved." : "2025 Burak Fevzi Kar. Tüm Hakları Gizlidir."}</p>
       <div className={`social-icons ${theme === "dark" ? "dark-theme" : "light-theme"}`}>
         <a href="https://www.linkedin.com/in/burak-fevzi-kar-2903b3201/" target="_blank" rel="noreferrer">
           <FaLinkedin />
